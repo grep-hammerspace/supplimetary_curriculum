ffd04d741320d4e0f20ff13c406cd0072c3e5860
Asad <asadyezdan@gmail.com>
2026-06-01 09:44:03 +0100

    progress

--- module4/hashmap/HashTable.go
+++ module4/hashmap/HashTable.go
@@ -1,13 +1,12 @@
 package hashmap
 
 import (
+	"fmt"
 	"hash/fnv"
-
-	ll "github.com/grep-hammerspace/coding-curriculum/module2/linkedlist"
 )
 
 type HashTable[K any, V any] struct {
-	Buckets    []ll.LinkedList[V]
+	Buckets    [][]HashNode[K, V]
 	LoadFactor float64
 	capacity   int
 }
@@ -16,23 +15,34 @@ type HashNode[K any, V any] struct {
 	Key   K
 	Value V
 	Hash  uint32 // Avoid recomputing each time
-	Next  *HashNode[K, V]
 }
 
 func (h *HashTable[K, V]) Put(key K, value V) {
-	bucket := hash(key) % 16
-	//Get corresponding bucket
+	// Determine bucket
+	keyHash := hash(key)
+	bucketNumber := keyHash % 16
+
 	//write to bucket
+	bucket := h.Buckets[bucketNumber]
+	newHashNode := HashNode[K, V]{key, value, keyHash}
+	bucket = append(bucket, newHashNode)
 }
 
-func (h *HashTable[K, V]) Get(K) V {
-	// Hash input
+func (h *HashTable[K, V]) Get(key K) V {
+	// Get correct bucket
+	bucket := h.Buckets[hash(key)%16]
+
 	// Go to bucket, traverse until you find K
+	for _, node := range bucket {
+		if node.Key == key {
+			return node.Value
+		}
+	}
 
 }
 
-func hash[K ~string](obj K) uint32 {
+func hash[K any](obj K) uint32 {
 	h := fnv.New32a()
-	h.Write([]byte(obj))
+	h.Write([]byte(fmt.Sprint(obj)))
 	return h.Sum32()
 }
