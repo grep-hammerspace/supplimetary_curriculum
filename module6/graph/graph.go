600cdece7f38762034981369ce3f59fa24164252
Asad <yezdanasad@gmail.com>
2026-08-24 09:33:08 +0100

    use adjacency graph

--- module6/graph/graph.go
+++ module6/graph/graph.go
@@ -1,43 +1,18 @@
 package graph
 
-type Node[T any] struct {
-	Neighbours []*Node[T]
-	Value      T
-}
-
-// Link a node to an existing one, returns pointer to new node
-func (n *Node[T]) Link(value T) *Node[T] {
-	newNode := Node[T]{
-		Neighbours: make([]*Node[T], 0),
-		Value:      value,
-	}
-
-	n.Neighbours = append(n.Neighbours, &newNode)
-
-	return &newNode
-}
+// Graph represented internally as adjacency matrix.
+// Adjacency matrix is a slices of slices, each must grow anytime we append a
 
 type Graph[T any] struct {
-	Root *Node[T]
+	Capacity int
+	Size     int
+	Matrix   [32][32]*T
 }
 
-
-
-// Create an empty graph with a single root node.
-func New[T any](value T) *Graph[T] {
-	root := Node[T]{
-		Neighbours: make([]*Node[T], 0),
-		Value: value,
-	}
-
-	graph := Graph[T]{
-		Root: &root,
-	}
-
-	return &graph
+func New[T any](capacity int) *Graph[T] {
+	return &Graph[T]{}
 }
 
-// Do graphs have root nodes?
-// yes they do, otherwise how would you do a depth first search or a breadth first search, you gotta start somewhere
-// How could you print the shape of a graph to terminal.
-// This isnt as easy, drawing lines would be really tricky, so maybe some kinda numerical represnetation. - surprisingly enough, and adjacecny matrix is what they suggested, whod have thunk it?
+func (g *Graph[T]) Add(value T) {
+
+}
